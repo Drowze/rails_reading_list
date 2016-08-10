5f07f12282abaefa793397820d07d5c912ea1f24
giba <gibim6@gmail.com>
2016-08-10 15:29:21 -0300

    SEARCH REVAMPED OMG

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,15 +2,22 @@ class Book < ApplicationRecord
 
 	scope :finished, ->{ where.not(finished_on: nil) }
 	scope :recent, ->{ where('finished_on > ?', 2.days.ago)}
-	# scope :search, ->(keyword){ where(title:keyword) if keyword.present? }
+	scope :search, ->(keyword){ where('keywords LIKE ?', "%#{keyword.downcase}%") if keyword.present? }
 
-	def self.search(keyword)
-		keyword.present? ? where(title: keyword) : all
-	end
+	before_save :set_keywords
+
+	# def self.search(keyword) # THIS WORKS BEAUTIFULY
+	# 	keyword.present? ? where(title: keyword) : all
+	# end
 
 	def finished?
 		finished_on.present?
 	end
+
+	protected
+		def set_keywords
+			self.keywords = [title, author, description].map(&:downcase).join(' ')
+		end
 
 	# def self.recent # THAT WORKS WHOA
 	# 	# where('finished_on > ?', 2.days.ago) 
