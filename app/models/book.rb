e5f6faa2cf697a6ed390b16262cc44ca59f5bf24
giba <gibim6@gmail.com>
2016-08-10 14:59:11 -0300

    Initial approach to search (aka poor man's search)

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,14 +1,19 @@
 class Book < ApplicationRecord
 
 	scope :finished, ->{ where.not(finished_on: nil) }
-	# scope :recent, ->{ where('finished_on > ?', 2.days.ago)}
+	scope :recent, ->{ where('finished_on > ?', 2.days.ago)}
+	# scope :search, ->(keyword){ where(title:keyword) if keyword.present? }
+
+	def self.search(keyword)
+		keyword.present? ? where(title: keyword) : all
+	end
+
 	def finished?
 		finished_on.present?
 	end
 
-	def self.recent
-		where('finished_on > ?', 2.days.ago)
-		# where(finished_on > 2.days.ago)
-		
-	end
+	# def self.recent # THAT WORKS WHOA
+	# 	# where('finished_on > ?', 2.days.ago) 
+
+	# end
 end
