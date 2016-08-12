0bb23f32f5c1173fd0d4edbeff27ea7f79a8675e
giba <gibim6@gmail.com>
2016-08-12 14:38:54 -0300

    simple devise for admin creating and editing BOOOOOOOKS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def authorize_admin
+  	redirect_to :back, :status => 301 unless admin_signed_in?
+  end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_admin, only: [:create, :update] ##  ~kinda works
 
   # GET /books
   # GET /books.json
